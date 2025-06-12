e576ed13c8f23a63f9f3ac70290866e85f1aa792
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-12 14:46:25 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Jokes from './pages/Jokes'
+import UsersPage from './pages/UsersPage'
 function App() {
 
     return (
         <>
-
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<MainLayout />}>
+                        <Route index element={<UsersPage />}/>
+                        <Route path='/users' element={<UsersPage />} />
+                        <Route path='/jokes' element={<Jokes />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,11 @@
-
+import { Outlet, Link } from "react-router-dom"
 const MainLayout = () => {
   return (
     <div>
+        <nav>
+            <Link to='/users'>Vartotojai</Link> | <Link to='/jokes'>Juokeliai</Link>
+        </nav>
+        <Outlet />
     </div>
   )
 }
