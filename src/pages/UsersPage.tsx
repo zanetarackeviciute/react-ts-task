a11f6e4c7414a18bc5f404e328841b32f5c6f863
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-13 15:24:44 +0300

    Add initial user logic; load users from localStorage or set default users

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,6 +1,57 @@
+import { useEffect, useState } from "react"
+
 const UsersPage = () => {
+    interface User {
+        id: number;
+        name: string;
+        position: string;
+        gender: string;
+        age: number;
+    }
+
+    const [users, setUsers] = useState<User[]>([])
+
+    const storedUsers = localStorage.getItem('users')
+
+   
+    
+    useEffect(() => {
+        if (storedUsers != null) {
+        setUsers(JSON.parse(storedUsers))
+        } else {
+            setUsers([
+                {
+                    id: 1, 
+                    name: 'Jonas', 
+                    position: 'Duomenų analitikas', 
+                    gender: 'Vyras', 
+                    age: 41
+                },
+                {
+                    id: 2,
+                    name: 'Lina',
+                    position: 'Front-End programuotoja',
+                    gender: 'Moteris',
+                    age: 32
+                },
+                {
+                    id: 3,
+                    name: 'Tomas',
+                    position: 'Java programuotojas',
+                    gender: 'Vyras',
+                    age: 20
+                }
+            ])
+        } 
+    }, [])
+
     return (
-        <div>UsersPage</div>
+        <div>
+            <h1>Vartotojų sąrašas</h1>
+            <ul>
+                {}
+            </ul>
+        </div>
     )
 }
 
