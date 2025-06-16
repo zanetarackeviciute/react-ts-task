585d4cab6a03a56b7ba6770034e087c7c29aa3c6
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-16 12:38:08 +0300

    Add jokes saving

--- src/pages/Jokes.tsx
+++ src/pages/Jokes.tsx
@@ -1,21 +1,41 @@
 import { useState, useEffect } from "react"
 const Jokes = () => {
-    const [joke, setJoke] = useState<string>('')
-    const [time, setTime] = useState<string>('')
+
+    interface Jokes {
+        joke: string;
+        date: string;
+    }
+
+    const storedJokes = localStorage.getItem('JokesList')
     
+    const [newJoke, setNewJoke] = useState<string>('')
+    const [newTime, setNewTime] = useState<string>('')
+
+    const [jokesList, setJokesList] = useState<Jokes[]>([])
+
+    useEffect(() => {
+        if (storedJokes) {
+            setJokesList(JSON.parse(storedJokes))
+        }
+    } ,[])
 
     useEffect(() => {
         async function getAjoke() { 
-            fetch('https://api.chucknorris.io/jokes/random?category=dev')
             const response = await fetch('https://api.chucknorris.io/jokes/random?category=dev')
             const data = await response.json()
-            setJoke(data.value)
-            setTime(new Date().toLocaleString())
+
+            const gottenJoke = {
+                joke: data.value,
+                date: new Date().toLocaleString()
+            }
+
+            setNewJoke(gottenJoke.joke)
+            setNewTime(gottenJoke.date)
+
+            setJokesList(oldList => [...oldList, gottenJoke])
         }
 
         getAjoke() 
-
-        
 
         const interval = setInterval(getAjoke, 15000)
 
@@ -23,13 +43,26 @@ const Jokes = () => {
 
     }, [])
 
+    useEffect(() => {
+        localStorage.setItem('JokesList', JSON.stringify(jokesList))
+    }, [jokesList])
+
     return (
         <div>
-            <h1>Dienos juokelis!</h1>
-            <p>{joke}</p>
-            <p>{time}</p>
+            <h2>Naujausias juokelis!</h2>
+            <p>{newJoke}</p>
+            <p>{newTime}</p>
+            {
+                jokesList.map((joky) => {
+                    return (
+                        <>
+                            <p>{joky.joke}</p>
+                            <p>{joky.date}</p>
+                        </>
+                    )
+                })
+            }
         </div>
     )
-
 }
 export default Jokes
