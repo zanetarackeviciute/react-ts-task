95ba80c057a9ba3f020cc8e56edd0e710b8f1aa4
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-06-16 11:16:28 +0300

    add async function - getting a joke

--- src/pages/Jokes.tsx
+++ src/pages/Jokes.tsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react"
 const Jokes = () => {
+    const [joke, setJoke] = useState<string>('')
+    const [time, setTime] = useState<string>('')
+
+    useEffect(() => {
+        async function getAjoke() { 
+            fetch('https://api.chucknorris.io/jokes/random?category=dev')
+            const response = await fetch('https://api.chucknorris.io/jokes/random?category=dev')
+            const data = await response.json()
+            setJoke(data.value)
+            setTime(new Date().toLocaleString())
+        }
+
+        getAjoke() 
+
+    }, [])
+
     return (
         <div>Jokes</div>
     )
-}
 
+}
 export default Jokes
